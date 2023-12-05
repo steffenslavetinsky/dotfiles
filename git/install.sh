d08044784926350e896cee4d8762532c5d7f46b4
Steffen Slavetinsky <steffen.slavetinsky@gmail.com>
2023-12-05 19:56:59 +0100

    set default user password and branch for git

---
+++ git/install.sh
@@ -0,0 +1,6 @@
+# setup git config
+#
+git config --global init.defaultBranch main
+
+git config --global user.name "Steffen Slavetinsky"
+git config --global user.email "steffen.slavetinsky@gmail.com"
