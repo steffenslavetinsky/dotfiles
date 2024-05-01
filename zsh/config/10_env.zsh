ad1ecc4e272462413801797c4f33d6040643ad32
Steffen Slavetinsky <steffen.slavetinsky@gmail.com>
2024-05-01 23:25:09 +0200

    remove spotlight stuff

--- zsh/config/10_env.zsh
+++ zsh/config/10_env.zsh
@@ -2,7 +2,4 @@ export PATH="$PATH:$DOTFILES_ROOT/bin"
 
 export UNAME="$(os_name)"
 
-export SPOTLIGHT_LICENSE_PATH=/Users/steffen/Documents/Renumics/projects/spotlight-pro/data/license-keys/developer-license.key
-export SPOTLIGHT_OPT_OUT=true
-
 export SSH_CONFIG="$DOTFILES_ROOT/ssh/configs/home"
